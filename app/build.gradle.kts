b42f6af526dec0a5085d9152f45eb05e4756fc37
hoseinhaghighi <80756114+hoseinhaghighi@users.noreply.github.com>
2026-09-14 06:07:21 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,23 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "ir.gogorilla.admin"; compileSdk = 35
-    defaultConfig { applicationId = "ir.gogorilla.admin"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "ir.gogorilla.admin"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "ir.gogorilla.admin"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
